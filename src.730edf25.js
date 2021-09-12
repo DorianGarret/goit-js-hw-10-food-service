95631f8ad9fb05cb1564255a8ce63e6504c7bf71
DorianGarret <89302676+DorianGarret@users.noreply.github.com>
2021-09-12 01:11:28 +0000

    Deploying to gh-pages from @ DorianGarret/goit-js-hw-10-food-service@f8fdf788bd1f47fe2171c1f76f720279c0b85cb3 🚀

--- src.730edf25.js
+++ src.730edf25.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
+},{}],"JIhF":[function(require,module,exports) {
+const e={switcher:document.getElementById("theme-switch-toggle"),body:document.body},{switcher:t,body:c}=e,n={LIGHT:"light-theme",DARK:"dark-theme"},{LIGHT:o,DARK:d}=n,s="current-theme";function a({target:e}){e.checked?i():h()}function h(){r(d,o),t.checked=!1}function i(){r(o,d),t.checked=!0}function r(e,t){c.classList.remove(e),c.classList.add(t),localStorage.setItem(s,t)}t.addEventListener("change",a);const l=({DARK:e})=>{switch(localStorage.getItem(s)){case e:i();break;default:h()}};l(n);
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10-food-service/src.3be23213.js.map
+"use strict";require("./sass/main.scss"),require("./js/markup"),require("./js/theme-switch");
+},{"./sass/main.scss":"clu1","./js/markup":"clu1","./js/theme-switch":"JIhF"}]},{},["Focm"], null)
+//# sourceMappingURL=/goit-js-hw-10-food-service/src.730edf25.js.map
